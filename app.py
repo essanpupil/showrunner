9356147743320021fc9b66823639afcda7ffb272
Ikhsan Noor Rosyidin <ikhsan@jublia.com>
2018-11-06 23:07:49 +0700

    dispaly chat via websocket

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "you-know-who"
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -8,5 +11,15 @@ def index():
     return render_template('index.html')
 
 
+def message_received(methods=['GET', 'POST']):
+    print('message was received!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit("my response", json, callback=message_received)
+
+
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app, debug=True)
